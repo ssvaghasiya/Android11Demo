77f4c095dbf23eda1bbfb39738bdf20feba8edac
nikvagh <67430636+nikvagh@users.noreply.github.com>
2021-04-03 13:43:17 +0530

    initial

---
+++ app/src/main/java/com/example/demoa11/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.example.demoa11
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import kotlin.concurrent.thread
+
+const val PICK_FILE = 1
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkPermission()
+        pickFile.setOnClickListener {
+            pickFileAndCopyUriToExternalFilesDir()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        val permissionsToRequire = ArrayList<String>()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsToRequire.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsToRequire.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (permissionsToRequire.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequire.toTypedArray(), 0)
+            return true
+        }
+        return false
+    }
+
+    private fun pickFileAndCopyUriToExternalFilesDir() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*"
+        startActivityForResult(intent, PICK_FILE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You must allow all the permissions.", Toast.LENGTH_SHORT)
+                        .show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PICK_FILE -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val uri = data.data
+                    if (uri != null) {
+                        val fileName = getFileNameByUri(uri)
+                        copyUriToExternalFilesDir(uri, fileName)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getFileNameByUri(uri: Uri): String {
+        var fileName = System.currentTimeMillis().toString()
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        if (cursor != null && cursor.count > 0) {
+            cursor.moveToFirst()
+            fileName =
+                cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
+            cursor.close()
+        }
+        return fileName
+    }
+
+    private fun copyUriToExternalFilesDir(uri: Uri, fileName: String) {
+        thread {
+            val inputStream = contentResolver.openInputStream(uri)
+            val tempDir = getExternalFilesDir("temp")
+            if (inputStream != null && tempDir != null) {
+                val file = File("$tempDir/$fileName")
+                val fos = FileOutputStream(file)
+                val bis = BufferedInputStream(inputStream)
+                val bos = BufferedOutputStream(fos)
+                val byteArray = ByteArray(1024)
+                var bytes = bis.read(byteArray)
+                while (bytes > 0) {
+                    bos.write(byteArray, 0, bytes)
+                    bos.flush()
+                    bytes = bis.read(byteArray)
+                }
+                bos.close()
+                fos.close()
+                runOnUiThread {
+                    Toast.makeText(this, "Copy file into $tempDir succeeded.", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
+    }
+}
